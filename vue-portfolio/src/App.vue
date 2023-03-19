5e2f4d10f307427edc6c747871a3ec9cce5b26e2
umingming <u_0112@naver.com>
2023-03-19 15:05:01 +0900

    feat: Apply router to header

--- vue-portfolio/src/App.vue
+++ vue-portfolio/src/App.vue
@@ -1,11 +1,9 @@
 <template>
     <div id="app wrap">
         <app-header></app-header>
-        <main-view></main-view>
-        <about-view></about-view>
-        <skill-view></skill-view>
-        <project-view></project-view>
-        <contact-view></contact-view>
+        <transition name="fade" mode="out-in">
+            <router-view></router-view>
+        </transition>
         <app-footer></app-footer>
     </div>
 </template>
@@ -13,23 +11,22 @@
 <script>
 import AppHeader from "@/components/AppHeader.vue";
 import AppFooter from "@/components/AppFooter.vue";
-import MainView from "@/views/MainView.vue";
-import AboutView from "@/views/AboutView.vue";
-import SkillView from "@/views/SkillView.vue";
-import ProjectView from "@/views/ProjectView.vue";
-import ContactView from "@/views/ContactView.vue";
 export default {
     name: "App",
     components: {
         AppHeader,
         AppFooter,
-        MainView,
-        AboutView,
-        SkillView,
-        ProjectView,
-        ContactView,
     },
 };
 </script>
 
-<style></style>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
