2e131e457b7f808b3e73d1a94d90f98778e97aa1
umingming <u_0112@naver.com>
2023-04-08 18:30:20 +0900

    feat: Update introduction

--- vue-portfolio/src/components/main/MainIntro.vue
+++ vue-portfolio/src/components/main/MainIntro.vue
@@ -11,7 +11,13 @@
             <div class="img">
                 <img :src="intro.img" />
             </div>
-            <div class="txt" v-html="intro.txt"></div>
+            <div class="txt">
+                <div v-html="intro.txt"></div>
+                <button @mouseover="hoveredIndex = index">
+                    <label>{{ intro.btn.label }}</label>
+                </button>
+                <i class="bi bi-cursor-fill"></i>
+            </div>
         </div>
     </div>
 </template>
@@ -19,11 +25,17 @@
 <script>
 import { INTRO } from "@/constants/main.js";
 export default {
+    data() {
+        return {
+            hoveredIndex: -1,
+        };
+    },
     computed: {
         introList() {
             return INTRO;
         },
     },
+    methods: {},
 };
 </script>
 
