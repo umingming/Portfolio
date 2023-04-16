89a84f1ce5776155e1f8fec59a94084d135b5b22
umingming <u_0112@naver.com>
2023-04-16 16:59:33 +0900

    feat: Update viewport

--- portfolio-vue/src/components/main/MainIntro.vue
+++ portfolio-vue/src/components/main/MainIntro.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="main-intro">
-        <div class="title" :class="{ 'slide-down': inViewport }">
+        <div class="title" :class="{ 'slide-down': $inViewport }">
             <h2>ABOUT ME</h2>
         </div>
         <div v-for="intro of INTROS" :key="intro.id" :id="intro.id">
-            <div class="img" :class="{ 'slide-left': inViewport }">
+            <div class="img" :class="{ 'slide-left': $inViewport }">
                 <img :src="intro.img" />
             </div>
-            <div class="txt" :class="{ 'slide-right': inViewport }">
+            <div class="txt" :class="{ 'slide-right': $inViewport }">
                 <div v-html="intro.txt"></div>
                 <base-button v-bind="intro"></base-button>
             </div>
@@ -18,14 +18,15 @@
 <script>
 import { SLIDES, INTROS } from "@/constants/main.js";
 import BaseButton from "@/components/base/BaseButton.vue";
+import viewportMixin from "@/mixins/viewportMixin.js";
 
 export default {
     components: { BaseButton },
+    mixins: [viewportMixin],
     data() {
         return {
             INTROS,
             SLIDES,
-            inViewport: false,
         };
     },
 };
